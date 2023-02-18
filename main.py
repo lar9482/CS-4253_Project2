09f499395b98a82180c9f57d0d47a5e68b4fd315
Luke Runnels <lar9482@utulsa.edu>
2023-02-18 12:51:26 -0600

    wrote basic io for loading on a cp graph

---
+++ main.py
@@ -0,0 +1,12 @@
+import json
+from utils.file_io import gen_gcp, load_gcp
+
+def main():
+    print()
+    # gen_gcp()
+    load_gcp()
+
+
+
+if __name__=="__main__":
+    main()
---
+++ utils/file_io.py
@@ -0,0 +1,27 @@
+import json
+from utils.gen_gcp import draw, gen
+
+def gen_gcp(num_points = 10, file_path = 'gcp.json'):
+    print("Generating a planar graph with {} points...".format(num_points))
+    (x, lines) = gen(num_points=num_points)
+    draw(x, lines)
+
+    print("Writing to '{}'...".format(file_path))
+    with open(file_path, 'w') as f:
+        f.write(json.dumps({
+            'num_points': len(x),
+            'points': { p.id: (p.x, p.y) for p in x },
+            'edges': [ (line.p1.id, line.p2.id) for line in lines ]
+        }, indent=2))
+    print("""Done! You can now import the results into a Python script with the code:
+
+import json
+
+with open('{}', r) as f:
+    data = json.load(f)
+""".format(file_path))
+
+def load_gcp(file_path = 'gcp.json'):
+    with open('gcp.json', 'r') as f:
+        data = json.load(f)
+        print()
