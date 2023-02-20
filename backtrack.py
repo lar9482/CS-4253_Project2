7051dc1958480c80da1e1dff280a0ffbb4b8133f
Luke Runnels <lar9482@utulsa.edu>
2023-02-19 21:09:05 -0600

    Basic backtracking without heuristics is working

--- backtrack.py
+++ backtrack.py
@@ -7,7 +7,7 @@ def backtrack(CSP = mcp_csp, Order_Method = random_method, Inference_Method = de
         return CSP
     
     variable = Order_Method(CSP)
-
+    print(variable)
     for domain_value in CSP.domain[variable]:
         if CSP.constraint_consistent(variable, domain_value):
             #Add {var = value} to assignment
--- main.py
+++ main.py
@@ -5,25 +5,33 @@ from order_method import random_method
 from backtrack import backtrack
 
 def test_mcp():
-    gen_mcp(15)
+    size = 30
+    gen_mcp(size)
     mcp_csp = load_mcp()
 
     test = backtrack(mcp_csp)
+
+    for var in range(0, size):
+        print(test.constraint_consistent(var, test.assignment[var]))
     print()
 
 
 def test_sudoku():
     block_size = 3
-    # gen_sudoku(25, block_size)
+    gen_sudoku(40, block_size)
     sudoku_csp = load_sudoku()
 
-    print(random_method(sudoku_csp))
+    result = backtrack(sudoku_csp)
+    for row in range(0, block_size ** 2):
+        for col in range(0, block_size ** 2):
+            print(result.assignment[(row, col)], end=" ")
+        print()
     print()
 
 
 def main():
-    test_mcp()
-    # test_sudoku()
+    # test_mcp()
+    test_sudoku()
     print()
     
 
